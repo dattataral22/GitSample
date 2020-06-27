52aa94c33fcb23658a2ae551deacb8f89c4b5e00
Datta Taral <datta_taral@persistent.co.in>
2020-06-27 17:43:09 +0530

    Browserstac code

---
+++ src/test/java/com/org/browerserstack/BrowserTest.java
@@ -0,0 +1,55 @@
+package com.org.browerserstack;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+
+public class BrowserTest 
+{
+	
+	 public static final String USERNAME = "dattataral1";
+	  public static final String AUTOMATE_KEY = "7Kta7iCixxKiAwPpqLRd";
+	  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+	 
+	@Test
+	public void start() throws MalformedURLException
+	{
+	
+		    DesiredCapabilities caps = new DesiredCapabilities();
+		    caps.setCapability("browserName", "chrome");
+		    caps.setCapability("browser_version", "83");
+		  //  caps.setCapability("device", "iPhone 8 Plus");
+		    caps.setCapability("realMobile", "true");
+		    caps.setCapability("os_version", "11");
+		    caps.setCapability("resolution", "1024x768");
+		    caps.setCapability("name", "Bstack-[Java] Sample Test");
+
+		    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+		    
+		    driver.get("http://demosite.center/wordpress/wp-login.php");
+		    WebElement username=driver.findElement(By.id("user_login"));
+			   username.sendKeys("admin");
+			   
+
+			   WebElement password=driver.findElement(By.id("user_pass"));
+			   password.sendKeys("demo123");
+			   
+
+			   WebElement btn=driver.findElement(By.id("wp-submit"));
+			   btn.click();
+			   
+
+		    System.out.println(driver.getTitle());
+		    driver.quit();
+	  }
+	}
+
+
